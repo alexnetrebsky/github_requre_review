092a8e887713e18ac49ffc239dbeeb4f12272abb
anetrebsky <alex.netrebsky@castle.co>
2016-09-17 08:56:54 +0400

    Provide GithubEventsController#create

--- app/controllers/github_events_controller.rb
+++ app/controllers/github_events_controller.rb
@@ -1,5 +1,13 @@
 class GithubEventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:create]
+
   def index
     @items = GithubEvent.all
+  end
+
+  def create
+    payload = JSON.parse(request.body.read)
+    GithubEvent.create({payload: payload})
+    render plain: 'OK'
   end
 end
