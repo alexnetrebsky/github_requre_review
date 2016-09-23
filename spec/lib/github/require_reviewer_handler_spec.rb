28b764cd37d0990cebe12d62ec56d491d13e66eb
anetrebsky <alex.netrebsky@castle.co>
2016-09-23 22:58:37 +0400

    Add test for require_reviewer_handler

--- spec/lib/github/require_reviewer_handler_spec.rb
+++ spec/lib/github/require_reviewer_handler_spec.rb
@@ -1,14 +1,33 @@
+require 'rails/all'
 require 'github_event_handler/handlers/require_reviewer_handler'
+require 'github/github_client'
+
 
 describe RequireReviewerHandler do
+  let(:event) { instance_spy('IssueCommentEvent') }
+  let!(:client) { instance_double('GithubClient',
+                                  pull_request_commits: [{'sha' => 'sha1'}],
+                                  create_status: []) }
 
-  it 'should do something' do
-    payload = {
-        'issue' => {'number' => 7},
-        'repository' => {'full_name' => 'user_name/repo'}
-    }
-    event = IssueCommentEvent.try_parse({'X-GitHub-Event' => 'issue_comment'}, payload)
+  before do
+    allow(GithubClient).to receive(:new).and_return(client)
+  end
 
-    result = RequireReviewerHandler.handle(event)
+  def set_comment(message)
+    allow(client).to receive(:issue_comments).and_return([{'body' => message}])
+  end
+
+  it 'sends failure status if there is not an appropriate comment' do
+    set_comment('not_approprite')
+
+    RequireReviewerHandler.new.handle(event)
+    expect(client).to have_received(:create_status).with(anything, 'sha1', 'failure', any_args)
+  end
+
+  it 'sends success status if there is an appropriate comment' do
+    set_comment([128077].pack('*U'))
+
+    RequireReviewerHandler.new.handle(event)
+    expect(client).to have_received(:create_status).with(anything, 'sha1', 'success', any_args)
   end
 end
