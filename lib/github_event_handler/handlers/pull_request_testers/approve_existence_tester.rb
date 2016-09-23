48ec377d589b97c18e36cf2ff940f9f7c9879015
anetrebsky <alex.netrebsky@castle.co>
2016-09-24 01:01:20 +0400

    Count number of needed approves

--- lib/github_event_handler/handlers/pull_request_testers/approve_existence_tester.rb
+++ lib/github_event_handler/handlers/pull_request_testers/approve_existence_tester.rb
@@ -1,6 +1,7 @@
 module PullRequestTesters
   class ApproveExistenceTester
     THUMBS_UP_SIGN_CODE = 128077
+    NUMBER_REQUIRED_APPROVES = 2
 
     def test(pull_request, client = nil)
       pull_request_number = pull_request.fetch(:number.to_s)
@@ -10,7 +11,15 @@ module PullRequestTesters
 
       number_approves = comments.select { |comment| comment[:body.to_s].unpack('U*').include?(THUMBS_UP_SIGN_CODE) }.count
 
-      return 'one review required' if number_approves == 0
+      if number_approves < NUMBER_REQUIRED_APPROVES
+        if (NUMBER_REQUIRED_APPROVES - number_approves > 1)
+          return "#{NUMBER_REQUIRED_APPROVES - number_approves} approves are required"
+        else
+          return "#{NUMBER_REQUIRED_APPROVES - number_approves} approve is required"
+        end
+      end
+
+      nil
     end
   end
 end
